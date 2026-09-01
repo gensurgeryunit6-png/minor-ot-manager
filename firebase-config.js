a5266e6ccff002a718d87b6307859abc34c0c8d8
gensurgeryunit6-png <gensurgeryunit6@gmail.com>
2026-09-02 00:17:52 +0530

    Load completed patient summary feature

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
 window.firebaseConfig={apiKey:"AIzaSyBhLTBL9j5ynCqLE_-0Bkj-qVY3v3SEz0g",authDomain:"minor-ot-manager.firebaseapp.com",projectId:"minor-ot-manager",storageBucket:"minor-ot-manager.firebasestorage.app",messagingSenderId:"240851011199",appId:"1:240851011199:web:3c3cf35c56751849eb214a"};
 if(window.firebase&&!firebase.apps.length)firebase.initializeApp(window.firebaseConfig);
 window.$=window.$||function(s){return document.querySelector(s)};
-(function(){var s=document.createElement('script');s.src='enhancements.js?v=8';s.async=false;document.head.appendChild(s)})();
+(function(){var s=document.createElement('script');s.src='enhancements.js?v=8';s.async=false;document.head.appendChild(s);var r=document.createElement('script');r.src='summary-report.js?v=1';r.async=false;document.head.appendChild(r)})();
